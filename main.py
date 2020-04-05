b4709e3e004d369b2e295a80acf704e848bc7dd8
Александр <xanter30@inbox.ru>
2020-04-05 12:09:39 +0400

    создание проекта

---
+++ main.py
@@ -0,0 +1,32 @@
+table = [[True, False, False, False],
+         [False, True, False, False],
+         [True, True, False, False],
+         [False, False, True, False],
+         [True, False, True, False],
+         [False, True, True, False],
+         [True, True, True, False],
+         [False, False, False, True],
+         [True, False, False, True],
+         [False, True, False, True],
+         [True, True, False, True],
+         [False, False, True, True],
+         [True, False, True, True],
+         [False, True, True, True],
+         [True, True, True, True]]
+
+def main(temp):
+    with open(temp, 'r' ,encoding = 'utf-8') as f:
+        mas = [line.strip() for line in f]
+        print(mas)
+        t = int(mas[0])
+
+
+
+
+
+
+
+
+
+#задать название файла и программа отработает
+main("small-test.in.txt")
