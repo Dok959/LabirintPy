ca82972ade2b381c1fba076230d5dc923aa5d5ad
Dok959 <eliseev99@inbox.ru>
2020-04-05 17:22:55 +0400

    начата разработка механизма обработки стен

--- main.py
+++ main.py
@@ -23,11 +23,49 @@ def main(file):
             func(i, temp)
 
 #обработка
-def func(n, temp):
-    print(temp)
+def func(n, path):
+    temp = ""
+    flag = False
+    for i in range(1, len(path)):
+        mas = []
+        if path[i] == "W":
+            if flag == False:
+                mas.append(True, True, False, False)
+            elif flag == True:
+                mas.append(False, False, True, True)
+        elif path[i] == "L":
+            if path[i-1] == "W":
+                mas.append(True, False, False, True)
+        else:
+            if path[i-1] == "W":
+                mas.append(True, False, True, False)
+            elif path[i-1] == "R":
+                mas.append(True, False, False, False)
+        temp += block(mas, table)
+        
 
+#определение границ
+def block(mas, table):
+    for i in range(len(table)):
+        if table[i] == mas:
+            index(i)
 
-
+#определение цифры или буквы
+def index(i):
+    if i == 10:
+        return "a"
+    elif i == 11:
+        return "b"
+    elif i == 12:
+        return "c"
+    elif i == 13:
+        return "d"
+    elif i == 14:
+        return "e"
+    elif i == 15:
+        return "f"
+    else:
+        return i
 
 #печать 
 def printf(n, t):
